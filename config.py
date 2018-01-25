03e6f70490a1e06ea21b306889a25e72aa871658
Brandon Berhent <bbedward@gmail.com>
2018-01-25 13:31:43 -0500

    Add examples to config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 twilio_sid = ''
 twilio_auth_token = ''
-twilio_phone_number = ''
+twilio_phone_number = '+13334445555'
 twilio_instructions_url = 'https://raw.githubusercontent.com/bbedward/Binance_API_Monitor/master/twilio_instructions.xml'
-numbers_to_dial = []
+numbers_to_dial = ['+12223334444','+14445556666']
